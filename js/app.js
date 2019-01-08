43de08841d94f454479b2eb7328fae713094929d
David Bogaerts <dbogaerts@gmail.com>
2019-01-08 13:37:47 -0600

    fixed player select styling and functionality with listeners to include click and enter keypress support

--- js/app.js
+++ js/app.js
@@ -34,7 +34,7 @@ const game = {
 		}
 	],
 	diceIn: 6,				//as dice are removed from play, this number is lowered.
-	playerCounter: 1,		//tracks what player's turn it is
+	playerCounter: null,	//tracks what player's turn it is
 	players: [],			//array holding players created by player class 			
 	rollDice() {
 		this.clearInPlayDice();				//clears board of in play dice before each roll
@@ -59,9 +59,18 @@ const game = {
 		}
 	}, 
 	startGame(numPlayers) {
-		for (let i = 1; i <= numPlayers; i++) {		//creates players
+		for (let i = 1; i <= numPlayers; i++) {		//creates players -- button press at beginning will pass num of players
 			this.players.push(new Player(`${i}`));
+			$('#score').append(`<p>${this.players[i-1].name}: <span id="score${i}">0</span></p>`); //adds a place for each player on leaderboard
 		}
+		this.playerCounter = 1; //game state (ie whose turn it is) will be controlled by playerCounter. Once game is initialized, the counter is set to 1 to denote it is player 1's turn.
+	},
+	resetGame() {		//resets game. will probably put reset button on page somewhere
+		this.diceIn = 6;
+		this.playerCounter = null;
+		this.players = [];
+		this.clearInPlayDice();
+		$('#score').empty();
 	}
 }
 	
@@ -85,6 +94,14 @@ $('body').on('click', (event) => {
 	if (event.target.innerText === 'Roll') {
 		game.rollDice();
 	}
+
+	if (event.target.innerText === 'Submit') {
+		event.preventDefault();
+		let numPlayers = $('input').val();
+		game.startGame(numPlayers);
+		$('#outer-player-select')[0].style.visibility = 'hidden';
+	}
 });
+
 
 
